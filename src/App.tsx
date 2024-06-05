503ac96a14b1be163763e80701bc7f4789c105cd
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2024-06-05 21:54:15 +0530

    Update App.tsx
    Unused files removed

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
-// import Integrations from './pages/Integrations';
-// import Settings from './pages/Settings';
-// import Account from './pages/Account';
-// import SignIn from './pages/SignIn';
-// import SignUp from './pages/SignUp';
-// import ResetPassword from './pages/ResetPassword';
+
 
 const App: React.FC = () => {
   return (
@@ -15,12 +10,7 @@ const App: React.FC = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        {/* <Route path="/integrations" element={<Integrations />} />
-        <Route path="/settings" element={<Settings />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/reset-password" element={<ResetPassword />} /> */}
+        
       </Routes>
     </Router>
   );
