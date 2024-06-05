5ccb144b0712a6bb05ccb2d4b11fa9639fc06ed5
Prasad Nathe <99172728+moto23@users.noreply.github.com>
2024-06-06 00:21:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Dashboard from './pages/Dashboard';
+import Settings from './pages/Settings';
+import SignUp from './pages/SignUp';
 
 
 const App: React.FC = () => {
@@ -10,7 +12,8 @@ const App: React.FC = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        
+        <Route path="/settings" component={Settings} />
+        <Route path="/signup" component={SignUp} />
       </Routes>
     </Router>
   );
